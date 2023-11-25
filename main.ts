cdcf77e180b5bd69b290ae24a07617de71f23eb3
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-11-25 21:40:00 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     qwiicjoystick.clearButtonStatus(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20))
 })
-radio.setGroup(22)
+radio.setTransmitPower(7)
+radio.setFrequencyBand(1)
+radio.setGroup(222)
 qwiicjoystick.beimStart(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20))
 loops.everyInterval(500, function () {
     radio.sendNumber(qwiicjoystick.readJoystick(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20)))
